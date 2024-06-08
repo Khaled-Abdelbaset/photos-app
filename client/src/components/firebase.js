3956425197e9759cda0228eb4521e248d52c888d
Khaled Abdelbaset <Khaledabdelbaset98@gmail.com>
2024-06-08 11:53:25 +0300

    adding firebase configurations

---
+++ client/src/components/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2H8ltRlxVbPeasy9spGR-uEnv5oEB0n8",
+  authDomain: "photos-app-57874.firebaseapp.com",
+  projectId: "photos-app-57874",
+  storageBucket: "photos-app-57874.appspot.com",
+  messagingSenderId: "836360920875",
+  appId: "1:836360920875:web:2df50819981b1ee00def6e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
